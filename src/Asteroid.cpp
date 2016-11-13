d3ab61211f721461bf0e87065247934f6ffbbf5d
ovidiu-giorgi <ovidiugiorgi@gmail.com>
2016-11-13 22:33:04 +0200

    Update Asteroid spawning
    Now asteroids spawn with a random size and color, between the
    12 possibilities

--- src/Asteroid.cpp
+++ src/Asteroid.cpp
@@ -6,23 +6,55 @@
 
 #include <cstdlib>
 
-#define IMG_WIDTH 45
-#define IMG_HEIGTH 40
-
 #define SPEED 450
+
+struct Pair {
+  int width;
+  int height;
+
+  Pair()
+  {
+    // Nothing
+  }
+
+  Pair(std::pair<int, int> p)
+  {
+    width = p.first;
+    height = p.second;
+  }
+};
 
 Asteroid::Asteroid() :
   _speed(SPEED)
 {
-  // std::cout << "Asteroid created\n";
+  // Set all the possible sizes of asteroids
+  Pair sizes[7];
+  sizes[1] = std::make_pair(51, 42);
+  sizes[2] = std::make_pair(60, 49);
+  sizes[3] = std::make_pair(45, 41);
+  sizes[4] = std::make_pair(49, 48);
+  sizes[5] = std::make_pair(43, 43);
+  sizes[6] = std::make_pair(45, 40);
 
-  // std::string meteorColor = rand() % 2 == 0 ? "Brown" : "Gray";
-  // std::string meteorSize = rand() % 2 == 0 ? "_big" : "_med";
+  // Pick a random size 
+  int rnd_size = rand() % 6 + 1;
 
-  _image = Resources::LoadImage("Assets/Asteroids/meteorBrown_med2.png");
-  _position = Vector2(rand() % (Screen::GetWidth() - IMG_WIDTH), Screen::GetHeight() + IMG_HEIGTH + rand() % 500);
-  _imgHeight = IMG_HEIGTH;
-  _imgWidth = IMG_WIDTH;
+  // Pick a random color
+  std::string rnd_color = rand() % 2 == 0 ? "brown" : "grey";
+
+  // Construct the filename for the asteroid image
+  std::string filename = "Assets/Asteroids/" + rnd_color + std::to_string(rnd_size) + ".png";
+
+  // Load the image
+  // _image = Resources::LoadImage("Assets/Asteroids/brown6.png");
+  // _imgWidth = 45;
+  // _imgHeight = 40;
+  _image = Resources::LoadImage(filename);
+  _imgWidth = sizes[rnd_size].height;
+  _imgHeight = sizes[rnd_size].width;
+  _position = Vector2(
+    rand() % (Screen::GetWidth() - _imgWidth),
+    Screen::GetHeight() + _imgHeight + rand() % 500);
 }
 
 Asteroid::~Asteroid()
